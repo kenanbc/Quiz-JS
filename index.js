85d0b0ba21640e5c8992f0dea242f76d5c947f87
Kenan Babic <kenanbc223@gmail.com>
2025-03-13 16:28:57 +0100

    Added final score container and resetGameParameters function

--- index.js
+++ index.js
@@ -21,6 +21,11 @@ const hardDificultyBtn = document.querySelector("#hard-dificulty-btn");
 const dificultyParagraph = document.querySelector("#dificulty-paragraph");
 
 const scoreTitle = document.querySelector("#score-title");
+const startAgainBtn = document.querySelector("#start-again-btn");
+const finishContainer = document.querySelector("#main-finish-container");
+const finishScore = document.querySelector("#finish-score");
+const finishNickname = document.querySelector("#finish-nickname");
+const finishQuote = document.querySelector("#finish-quote");
 
 const answerBtns = [...document.querySelectorAll(".answer-btn")];
 
@@ -36,6 +41,32 @@ let data;
 const setOfAnswers = [];
 let questionCounter = 0;
 let scoreCounter = 0;
+
+const victoryQuotes = [
+    "Knowledge is power – and I just proved it!",
+    "I came, I saw, I conquered… the quiz!",
+    "Brains over luck, every time!",
+    "Winning isn't everything, but it sure feels great!",
+    "Another victory for the curious mind!",
+    "Smart moves lead to sweet victories!",
+    "One step closer to becoming a trivia master!",
+    "I didn’t just guess – I knew it!",
+    "Wisdom always wins!",
+    "Quiz conquered. Next challenge?"
+];
+
+const failureQuotes = [
+    "Failure is just a step toward success!",
+    "I didn’t lose; I learned.",
+    "Mistakes are proof that I’m trying.",
+    "Even the best fail sometimes!",
+    "Next time, I'll do better!",
+    "Knowledge grows with every wrong answer.",
+    "Defeat today, victory tomorrow!",
+    "I may have lost, but I’m not giving up!",
+    "A wrong answer is just a lesson in disguise.",
+    "It’s not about how many times you fail, but how many times you rise!"
+];
 
 easyDificultyBtn.addEventListener("click", () => {
     choosenDificulty = 'easy';
@@ -75,28 +106,9 @@ numOfQuestionsSlider.addEventListener("input", () =>{
 exitBtn.addEventListener("click", () => {
     startGameDiv.style.display = 'flex';
     playGameDiv.style.display = 'none';
-    nickname.value = '';
-    numOfQuestionsSlider.value = SLIDER_VALUE;
-    numOfQuestions.textContent = SLIDER_VALUE;
-    choosenDificulty = DIFICULTY;
-    dificultyBtn.textContent = 'Choose Dificulty'
-    categoriesBtn.textContent = 'Choose Categories';
-    choosenCategories = CATEGORIES;
-    let selectedCategories = [...document.querySelectorAll("#categories-form input[type='checkbox']:checked")];
-    selectedCategories.forEach(checkbox => checkbox.checked = false);
-    setOfAnswers.length = 0;
-    questionCounter = 0;
-    scoreCounter = 0;
-    scoreTitle.textContent = SCORE_TITLE + scoreCounter;
-    answerBtns.forEach((btn) => {
-            btn.classList.remove("correct-answer-btn");
-            btn.classList.remove("wrong-answer-btn");
-            btn.disabled = false;
-    });
 
-    if (resetTimerFunc) {
-        resetTimerFunc.resetTimer();
-    }
+    resetGameParameters();
+    
 });
 
 startBtn.addEventListener("click", async () => {
@@ -130,10 +142,22 @@ const arrangeAllAnswers = (question) => {
 };
 
 const newQuestionLogic = () => {
-    if(questionCounter == numOfQuestionsSlider.value) return;
+    if(questionCounter == numOfQuestionsSlider.value){
+        playGameDiv.style.display = 'none';
+        finishContainer.style.display = 'flex';
+        finishNickname.textContent = nickname.value;
+        finishScore.textContent = `${scoreCounter}/${numOfQuestionsSlider.value}`
+        if(scoreCounter >= +numOfQuestionsSlider.value / 2){
+            finishQuote.textContent = victoryQuotes[parseInt(Math.random() * victoryQuotes.length)]
+        }
+        else{
+            finishQuote.textContent = failureQuotes[parseInt(Math.random() * failureQuotes.length)];
+        }
+        return;
+    }
     setOfAnswers.length = 0;
-    questionText.textContent = data[questionCounter].question.text;
     resetTimerFunc = runTimer(document.querySelector('.timer'));
+    questionText.textContent = data[questionCounter].question.text;
     questionNumber.textContent = `${questionCounter + 1}/${numOfQuestionsSlider.value}`;
     arrangeAllAnswers(data[questionCounter]);
     questionCounter++;
@@ -158,8 +182,39 @@ const btnEvents = () => {
                 scoreTitle.textContent = SCORE_TITLE + scoreCounter;
             }
 
-            await new Promise(resolve => setTimeout(resolve, 3000));
+            await new Promise(resolve => setTimeout(resolve, 2000));
             newQuestionLogic();
         });
     });
 };
+
+startAgainBtn.addEventListener("click", () => {
+    finishContainer.style.display = 'none';
+    startGameDiv.style.display = 'flex';
+    resetGameParameters();
+});
+
+const resetGameParameters = () => {
+    nickname.value = '';
+    numOfQuestionsSlider.value = SLIDER_VALUE;
+    numOfQuestions.textContent = SLIDER_VALUE;
+    choosenDificulty = DIFICULTY;
+    dificultyBtn.textContent = 'Choose Dificulty'
+    categoriesBtn.textContent = 'Choose Categories';
+    choosenCategories = CATEGORIES;
+    let selectedCategories = [...document.querySelectorAll("#categories-form input[type='checkbox']:checked")];
+    selectedCategories.forEach(checkbox => checkbox.checked = false);
+    setOfAnswers.length = 0;
+    questionCounter = 0;
+    scoreCounter = 0;
+    scoreTitle.textContent = SCORE_TITLE + scoreCounter;
+    answerBtns.forEach((btn) => {
+            btn.classList.remove("correct-answer-btn");
+            btn.classList.remove("wrong-answer-btn");
+            btn.disabled = false;
+    });
+
+    if (resetTimerFunc) {
+        resetTimerFunc.resetTimer();
+    }
+}
